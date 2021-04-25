7dd56c627f62c587a6092c465331dc89b5bc459a
Onur Demir <aaonurdemir@gmail.com>
2021-04-25 21:48:52 +0300

    Add guidance

--- Balancer-git/Program.cs
+++ Balancer-git/Program.cs
@@ -48,6 +48,7 @@ namespace IngameScript
         RectangleF _viewportLargePanel;
         RectangleF _viewportLeftPanel;
         IMyCockpit _cockpit;
+        bool isActive;
       
 
 
@@ -55,7 +56,7 @@ namespace IngameScript
         // Script constructor
         public Program()
         {
-
+            isActive = false;
             // Me is the programmable block which is running this script.
             // Retrieve the Large Display, which is the first surface
             //_drawingSurface = Me.GetSurface(0);
@@ -70,7 +71,7 @@ namespace IngameScript
 
 
             // Set the continuous update frequency of this script
-            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+            Runtime.UpdateFrequency = UpdateFrequency.Update100 | UpdateFrequency.Update10;
 
             // Calculate the viewport offset by centering the surface size onto the texture size
             _viewportLargePanel = new RectangleF(
@@ -113,6 +114,18 @@ namespace IngameScript
             // can be removed if not needed.
 
             // Begin a new frame
+            if ((updateSource & (UpdateType.Trigger | UpdateType.Terminal)) != 0)
+            {
+                if(argument == "1")
+                {
+                    isActive = true;
+                }
+                else {
+                    isActive = false;
+                }
+            }
+
+
             if ((updateSource & UpdateType.Update100) != 0)
             {
                 ScanGrid();
@@ -131,9 +144,22 @@ namespace IngameScript
 
                 Situation.RefreshParameters();
             }
+            if ((updateSource & UpdateType.Update10) != 0 && isActive)
+            {
+                Guidance.Set();
+                Guidance.Tick();
+                if (Guidance.Done())
+                {                    
+                    Guidance.Release();
+                }
+            }
+            if ((updateSource & UpdateType.Update10) != 0 && !isActive)
+            {
+                Guidance.Release();
+            }
 
-            
-            
+
+
 
         }
         public void PrepareTextSurfaceForSprites(IMyTextSurface textSurface)
@@ -279,7 +305,7 @@ namespace IngameScript
 
         private static float HORIZONT_CHECK_DISTANCE = 2000.0f;
         private static float DISTANCE_TO_GROUND_IGNORE_PLANET = 1.2f * HORIZONT_CHECK_DISTANCE;
-        private static float MAX_SPEED = 1.0f;
+        private static float MAX_SPEED = 50f;
 
         public static class Situation
         {
@@ -371,6 +397,247 @@ namespace IngameScript
                     gravityDownVector = downVector;
                     gravityUpVector = upVector;
                 }
+            }
+        }
+
+
+
+        private static float GUIDANCE_MIN_AIM_DISTANCE = 0.5f;
+        private static double GYRO_GAIN = 1.0;
+        private static double GYRO_MAX_ANGULAR_VELOCITY = Math.PI;
+        private static double TICK_TIME = 0.16666f;
+        private static float IDLE_POWER = 0.0000001f;
+
+        public static class Guidance
+        {
+            private static Vector3D desiredPosition = new Vector3D();
+            private static Vector3D desiredFront = new Vector3D();
+            private static Vector3D desiredUp = new Vector3D();
+            private static float desiredSpeed = MAX_SPEED;
+            public static void Set()
+            {
+                desiredPosition = Cockpit.block.GetPosition() + Cockpit.block.CubeGrid.WorldMatrix.GetDirectionVector(Base6Directions.Direction.Forward) * 1000f;
+                desiredFront = Cockpit.block.CubeGrid.WorldMatrix.GetDirectionVector(Base6Directions.Direction.Forward);
+                desiredUp = Cockpit.block.CubeGrid.WorldMatrix.GetDirectionVector(Base6Directions.Direction.Up);
+                desiredSpeed = 2f;
+            }
+            public static void Release()
+            {
+                foreach (IMyGyro gyro in AppGridBlocks.gyros)
+                {
+                    gyro.GyroOverride = false;
+                }
+                foreach (IMyThrust thruster in AppGridBlocks.thrusters)
+                {
+                    thruster.ThrustOverride = 0;
+                }
+            }
+            public static void Tick()
+            {
+                Guidance.StanceTick();
+                Guidance.GyroTick();
+                Guidance.ThrusterTick();
+            }
+            public static bool Done()
+            {
+                return worldVector.Length() < 0.05 && pathLen <= 0.1f;
+            }
+            private static Vector3D pathNormal, path, aimTarget, upVector, aimVector;
+            private static float pathLen;
+            private static void StanceTick()
+            {
+                path = desiredPosition - Situation.position;
+                pathLen = (float)path.Length();
+                pathNormal = Vector3D.Normalize(path);
+                if (desiredFront != Vector3D.Zero)
+                {
+                    aimTarget = Situation.position + desiredFront * Situation.radius;
+                }
+                else
+                {
+                    aimVector = (pathLen > GUIDANCE_MIN_AIM_DISTANCE) ? pathNormal : Situation.forwardVector;
+                    if (Situation.inGravity)
+                    {
+                        aimTarget = Situation.position + Vector3D.Normalize(Vector3D.ProjectOnPlane(ref aimVector, ref Situation.gravityUpVector)) * Situation.radius;
+                    }
+                    else
+                    {
+                        aimTarget = Situation.position + aimVector * Situation.radius;
+                    }
+                }
+                if (Situation.inGravity)
+                {
+                    upVector = (desiredUp == Vector3D.Zero) ? Situation.gravityUpVector : desiredUp;
+                }
+                else
+                {
+                    upVector = (desiredUp == Vector3D.Zero) ? Vector3D.Cross(aimVector, Situation.leftVector) : desiredUp;
+                }
+            }
+            private static Quaternion invQuat;
+            private static Vector3D direction, refVector, worldVector, localVector, realUpVect, realRightVect;
+            private static double azimuth, elevation, roll;
+            private static void GyroTick()
+            {
+                if (AppGridBlocks.gyros.Count == 0)
+                {
+                    return;
+                }
+                direction = Vector3D.Normalize(aimTarget - Situation.position);
+                invQuat = Quaternion.Inverse(Quaternion.CreateFromForwardUp(Situation.forwardVector, Situation.upVector));
+                refVector = Vector3D.Transform(direction, invQuat);
+                Vector3D.GetAzimuthAndElevation(refVector, out azimuth, out elevation);
+                realUpVect = Vector3D.ProjectOnPlane(ref upVector, ref direction);
+                realUpVect.Normalize();
+                realRightVect = Vector3D.Cross(direction, realUpVect);
+                realRightVect.Normalize();
+                roll = Vector3D.Dot(Situation.upVector, realRightVect);
+                worldVector = Vector3.Transform((new Vector3D(elevation, azimuth, roll)), Situation.orientation);
+                foreach (IMyGyro gyro in AppGridBlocks.gyros)
+                {
+                    localVector = Vector3.Transform(worldVector, Matrix.Transpose(gyro.WorldMatrix.GetOrientation()));
+                    gyro.Pitch = (float)MathHelper.Clamp((-localVector.X * GYRO_GAIN), -GYRO_MAX_ANGULAR_VELOCITY, GYRO_MAX_ANGULAR_VELOCITY);
+                    gyro.Yaw = (float)MathHelper.Clamp(((-localVector.Y) * GYRO_GAIN), -GYRO_MAX_ANGULAR_VELOCITY, GYRO_MAX_ANGULAR_VELOCITY);
+                    gyro.Roll = (float)MathHelper.Clamp(((-localVector.Z) * GYRO_GAIN), -GYRO_MAX_ANGULAR_VELOCITY, GYRO_MAX_ANGULAR_VELOCITY);
+                    gyro.GyroOverride = true;
+                }
+            }
+            private static float forwardChange, upChange, leftChange, applyPower, massFix;
+            private static Vector3D force, directVel, directNormal, indirectVel;
+            private static double ttt, maxFrc, maxVel, maxAcc, TIME_STEP = 2.5 * TICK_TIME, smooth;
+            private static float massA = 2000000.0f;
+            private static float massB = 5000.0f;
+            private static float massM = (massA - 2.0f * massB) / (massA - massB);
+            private static float massN = 1.0f / (massA - massB);
+            private static void ThrusterTick()
+            {
+                if (pathLen == 0.0f)
+                {
+                    return;
+                }
+                massFix = massM * Situation.mass + massN * Situation.mass * Situation.mass;
+
+                force = Situation.mass * Situation.naturalGravity;
+                directVel = Vector3D.ProjectOnVector(ref Situation.linearVelocity, ref pathNormal);
+                directNormal = Vector3D.Normalize(directVel);
+                if (!directNormal.IsValid())
+                {
+                    directNormal = Vector3D.Zero;
+                }
+                maxFrc = Situation.GetMaxThrust(pathNormal) - ((Vector3D.Dot(force, pathNormal) > 0) ? Vector3D.ProjectOnVector(ref force, ref pathNormal).Length() : 0.0);
+                maxVel = Math.Sqrt(2.0 * pathLen * maxFrc / massFix);
+                smooth = Math.Min(Math.Max((desiredSpeed + 1.0f - directVel.Length()) / 2.0f, 0.0f), 1.0f);
+                maxAcc = 1.0f + (maxFrc / massFix) * smooth * smooth * (3.0f - 2.0f * smooth);
+                ttt = Math.Max(TIME_STEP, Math.Abs(maxVel / maxAcc));
+                force += massFix * -2.0 * (pathNormal * pathLen / ttt / ttt - directNormal * directVel.Length() / ttt);
+                indirectVel = Vector3D.ProjectOnPlane(ref Situation.linearVelocity, ref pathNormal);
+                force += massFix * indirectVel / TIME_STEP;
+                forwardChange = (float)Vector3D.Dot(force, Situation.gridForwardVect);
+                upChange = (float)Vector3D.Dot(force, Situation.gridUpVect);
+                leftChange = (float)Vector3D.Dot(force, Situation.gridLeftVect);
+                foreach (IMyThrust thruster in AppGridBlocks.thrusters)
+                {
+                    if (!thruster.IsWorking)
+                    {
+                        thruster.ThrustOverridePercentage = 0;
+                        continue;
+                    }
+                    switch (thruster.Orientation.Forward)
+                    {
+                        case Base6Directions.Direction.Forward:
+                            thruster.ThrustOverridePercentage = ((forwardChange < 0) ? IDLE_POWER : (Guidance.Drain(ref forwardChange, thruster.MaxEffectiveThrust)));
+                            break;
+                        case Base6Directions.Direction.Backward:
+                            thruster.ThrustOverridePercentage = ((forwardChange > 0) ? IDLE_POWER : (Guidance.Drain(ref forwardChange, thruster.MaxEffectiveThrust)));
+                            break;
+                        case Base6Directions.Direction.Up:
+                            thruster.ThrustOverridePercentage = ((upChange < 0) ? IDLE_POWER : (Guidance.Drain(ref upChange, thruster.MaxEffectiveThrust)));
+                            break;
+                        case Base6Directions.Direction.Down:
+                            thruster.ThrustOverridePercentage = ((upChange > 0) ? IDLE_POWER : (Guidance.Drain(ref upChange, thruster.MaxEffectiveThrust)));
+                            break;
+                        case Base6Directions.Direction.Left:
+                            thruster.ThrustOverridePercentage = ((leftChange < 0) ? IDLE_POWER : (Guidance.Drain(ref leftChange, thruster.MaxEffectiveThrust)));
+                            break;
+                        case Base6Directions.Direction.Right:
+                            thruster.ThrustOverridePercentage = ((leftChange > 0) ? IDLE_POWER : (Guidance.Drain(ref leftChange, thruster.MaxEffectiveThrust)));
+                            break;
+                    }
+                }
+            }
+            private static float Drain(ref float remainingPower, float maxEffectiveThrust)
+            {
+                applyPower = Math.Min(Math.Abs(remainingPower), maxEffectiveThrust);
+                remainingPower = (remainingPower > 0) ? (remainingPower - applyPower) : (remainingPower + applyPower);
+                return Math.Max(applyPower / maxEffectiveThrust, IDLE_POWER);
+            }
+        }
+
+        public class Waypoint
+        {
+            public Stance stance;
+            public float maxSpeed;
+            public enum wpType
+            {
+                ALIGNING, DOCKING, UNDOCKING, CONVERGING, APPROACHING, NAVIGATING, TESTING, TAXIING
+            };
+            public wpType type;
+            public Waypoint(Stance s, float m, wpType wt)
+            {
+                stance = s;
+                maxSpeed = m;
+                type = wt;
+            }
+          
+            public static Waypoint FromString(string coordinates)
+            {
+                return new Waypoint(new Stance(Helper.UnserializeVector(coordinates), Vector3D.Zero, Vector3D.Zero), MAX_SPEED, wpType.CONVERGING);
+            }
+        }
+
+        public class Stance
+        {
+            public Vector3D position;
+            public Vector3D forward;
+            public Vector3D up;
+            public Stance(Vector3D p, Vector3D f, Vector3D u)
+            {
+                this.position = p;
+                this.forward = f;
+                this.up = u;
+            }
+        }
+
+        public static class Helper
+        {
+            public static string FormatedWaypoint(bool stance, int pos)
+            {
+                return (stance ? "Ori " : "Pos ") + (++pos).ToString("D2");
+            }
+            public static string Capitalize(string s)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    return string.Empty;
+                }
+                return s.First().ToString().ToUpper() + s.Substring(1).ToLower();
+            }
+            public static Vector3D UnserializeVector(string str)
+            {
+                var parts = str.Split(':');
+                Vector3D v = Vector3D.Zero;
+                if (parts.Length != 6)
+                {
+                    return v;
+                }
+                try
+                {
+                    v = new Vector3D(double.Parse(parts[2]), double.Parse(parts[3]), double.Parse(parts[4]));
+                }
+                catch
+                {
+                }
+                return v;
             }
         }
     }
